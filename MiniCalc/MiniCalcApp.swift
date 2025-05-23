061ab0d0cabcffac7ae1e03e5b2a1cdcaaa00afa
Julien Eyriès <julien@eyries.fr>
2025-05-23 16:18:41 +0200

    Initial Commit

---
+++ MiniCalc/MiniCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniCalcApp.swift
+//  MiniCalc
+//
+//  Created by Julien on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MiniCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
